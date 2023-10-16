25e427f5710392870750276145a2e40872542ade
anandkumarpatel1 <anandkumar7368970125@gmail.com>
2023-10-16 13:23:08 +0530

    .

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require("express");
 const app = express();
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const bodyParser = require("body-parser");
+const fileUpload = require('express-fileupload')
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "config/config.env" });
 }
 
 app.use(express.json());
-app.use(cors())
-app.use(express.urlencoded({ extended: true }));
+app.use("*",cors({
+  origin: true,
+  credentials: true,
+}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
 app.use(cookieParser())
 
 //imported routes
