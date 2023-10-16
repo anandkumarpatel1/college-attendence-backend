8c84479f47b73f5cacca786a6d1e1872be08908d
anandkumarpatel1 <anandkumar7368970125@gmail.com>
2023-10-16 08:00:50 +0530

    .

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "config/config.env" });
 }
 
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
